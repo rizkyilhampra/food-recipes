44faf0675755db1f3ef96e8f619d147cfc6f4dd8
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-02 03:09:56 +0800

    test; added home test

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Inertia\Testing\AssertableInertia;
+
+use function Pest\Laravel\get;
+
+it('can access homepage', function () {
+    $response = get('/');
+
+    $response->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Home'));
+});
