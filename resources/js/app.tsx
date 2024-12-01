2c16a63eb45f807751dabdc7f2696f86f6deb612
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-02 01:08:24 +0800

    chore: renamed to typescript and added setup for inertia react

---
+++ resources/js/app.tsx
@@ -0,0 +1,19 @@
+import './../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+import { Providers } from './components/providers';
+
+createInertiaApp({
+  resolve: (name) =>
+    resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+  setup({ el, App, props }) {
+    createRoot(el).render(
+      <Providers>
+        <App {...props}></App>
+      </Providers>
+    );
+  }
+});
