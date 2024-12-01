256e1e55d24933d0aee53d751dd304ec814c7451
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-02 03:09:50 +0800

    test: added user models test

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use function PHPUnit\Framework\assertEquals;
+
+it('user model has correct fillable properties', function () {
+    $user = new App\Models\User();
+
+    assertEquals([
+        'name',
+        'email',
+        'password',
+    ], $user->getFillable());
+});
+
+it('user model has correct hidden properties', function () {
+    $user = new App\Models\User();
+
+    assertEquals([
+        'password',
+        'remember_token',
+    ], $user->getHidden());
+});
+
+it('user model has correct casts properties', function () {
+    $user = new App\Models\User();
+
+    assertEquals([
+        'id' => 'int',
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ], $user->getCasts());
+});
