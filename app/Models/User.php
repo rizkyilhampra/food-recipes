628720d29d4aaf461e5769172c0285e280fd2637
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 06:55:55 +0800

    chore(user): mutate avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Services\Avatar;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -33,6 +35,18 @@ final class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return Attribute<string, never>
+     */
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => (new Avatar($this))->url()
+        );
+    }
 
     /**
      * Get the attributes that should be cast.
