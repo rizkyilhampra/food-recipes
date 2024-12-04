4db76a021023f4f3a32d9ded7eec50080f5ad9bc
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 06:58:24 +0800

    test: added AuthData

---
+++ tests/Unit/Data/AuthDataTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Data\AuthData;
+use App\Data\UserData;
+use App\Models\User;
+use App\Services\Avatar;
+
+it('have defined properties', function () {
+    $data = [
+        'user' => UserData::from([
+            'id' => 1,
+            'name' => 'rizky ilham',
+            'email' => 'rizkyilhamp16@gmail.com',
+            'avatar' => (new Avatar(User::factory()->make()))->url(),
+        ])->toArray(),
+    ];
+
+    $authData = AuthData::from($data);
+
+    expect($authData->toArray())->toBe($data);
+});
