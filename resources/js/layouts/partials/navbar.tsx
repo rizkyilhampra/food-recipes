f9c1d660589126e9dc3fceca2ab7ac6233a6b935
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 07:00:03 +0800

    chore(navbar): updated for follow latest types and added UserMenu

--- resources/js/layouts/partials/navbar.tsx
+++ resources/js/layouts/partials/navbar.tsx
@@ -1,9 +1,11 @@
-import { UserData } from '@/types';
+import { useTheme } from '@/components/theme-provider';
+import { AuthData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { ThemeSwitcher } from 'components/theme-switcher';
-import { IconBrandLaravel } from 'justd-icons';
+import { IconBrandJustd, IconBrandLaravel, IconColorSwatch, IconSettings } from 'justd-icons';
 import React from 'react';
-import { buttonStyles, Navbar, Separator } from 'ui';
+import { Selection } from 'react-aria-components';
+import { Avatar, buttonStyles, Menu, Navbar, Separator } from 'ui';
 
 const navigations = [
   {
@@ -15,7 +17,7 @@ const navigations = [
 
 export function AppNavbar({ children, ...props }: React.ComponentProps<typeof Navbar>) {
   const page = usePage();
-  const { user } = usePage<UserData>().props;
+  const { user } = usePage<AuthData>().props;
   const [isOpen, setIsOpen] = React.useState(false);
   React.useEffect(() => setIsOpen(false), [page.url]);
 
@@ -33,8 +35,8 @@ export function AppNavbar({ children, ...props }: React.ComponentProps<typeof Na
           ))}
         </Navbar.Section>
         <Navbar.Section className="hidden ml-auto gap-x-1 lg:flex">
-          <ThemeSwitcher />
-          <LoginButton />
+          {!user && <ThemeSwitcher />}
+          {user ? <UserMenu /> : <LoginButton />}
         </Navbar.Section>
       </Navbar.Nav>
 
@@ -47,8 +49,8 @@ export function AppNavbar({ children, ...props }: React.ComponentProps<typeof Na
           </Navbar.Logo>
         </Navbar.Flex>
         <Navbar.Flex className="gap-x-1">
-          <ThemeSwitcher />
-          <LoginButton />
+          {!user && <ThemeSwitcher />}
+          {user ? <UserMenu /> : <LoginButton />}
         </Navbar.Flex>
       </Navbar.Compact>
 
@@ -62,5 +64,74 @@ function LoginButton() {
     <Link href={route('login')} className={buttonStyles({ appearance: 'outline', size: 'small' })}>
       Login
     </Link>
+  );
+}
+
+function UserMenu() {
+  const { user } = usePage<AuthData>().props;
+  const { theme, setTheme } = useTheme();
+  const currentTheme = theme || 'system';
+  const [selectedTheme, setSelectedTheme] = React.useState<Selection>(new Set([currentTheme]));
+  return (
+    <Menu>
+      <Menu.Trigger aria-label="Open menu">
+        <Avatar status="online" size="medium" src={user.avatar} className="size-8" />
+      </Menu.Trigger>
+      <Menu.Content showArrow placement="bottom end" className="sm:min-w-56">
+        <Menu.Section>
+          <Menu.Header separator className="relative">
+            <div>{user.name}</div>
+            <div className="text-muted-fg font-normal text-sm whitespace-nowrap truncate pr-6">
+              {user.email}
+            </div>
+          </Menu.Header>
+        </Menu.Section>
+        <Menu.Item href={route('dashboard')}>Dashboard</Menu.Item>
+        <Menu.Item href={route('profile.edit')} className="justify-between">
+          Settings
+          <IconSettings />
+        </Menu.Item>
+        <Menu.Submenu>
+          <Menu.Item>Preferences</Menu.Item>
+          <Menu.Content
+            selectionMode="single"
+            selectedKeys={selectedTheme}
+            onSelectionChange={(keys) => {
+              setSelectedTheme(keys);
+              // @ts-ignore
+              setTheme(keys.has('system') ? 'system' : keys.has('dark') ? 'dark' : 'light');
+            }}
+            items={[
+              { name: 'Light', value: 'light' },
+              { name: 'Dark', value: 'dark' },
+              { name: 'System', value: 'system' }
+            ]}
+          >
+            {(item) => (
+              <Menu.Checkbox id={item.value} textValue={item.name}>
+                {item.name}
+              </Menu.Checkbox>
+            )}
+          </Menu.Content>
+        </Menu.Submenu>
+        <Menu.Separator />
+        <Menu.Item target="_blank" href="https://laravel.com" className="justify-between">
+          Documentation
+          <IconBrandLaravel />
+        </Menu.Item>
+        <Menu.Item target="_blank" href="https://getjustd.com" className="justify-between">
+          Components
+          <IconBrandJustd />
+        </Menu.Item>
+        <Menu.Item target="_blank" href="https://getjustd.com/colors" className="justify-between">
+          Colors
+          <IconColorSwatch />
+        </Menu.Item>
+        <Menu.Separator />
+        <Menu.Item routerOptions={{ method: 'post' }} href={route('logout')}>
+          <span>Logout</span>
+        </Menu.Item>
+      </Menu.Content>
+    </Menu>
   );
 }
