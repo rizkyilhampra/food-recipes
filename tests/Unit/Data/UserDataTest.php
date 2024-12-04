6c874cf2ea9f55dc0e630f9a17f450fdebb12e26
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 06:58:11 +0800

    test(UserData): added field avatar

--- tests/Unit/Data/UserDataTest.php
+++ tests/Unit/Data/UserDataTest.php
@@ -1,13 +1,17 @@
 <?php
 
 declare(strict_types=1);
+
 use App\Data\UserData;
+use App\Models\User;
+use App\Services\Avatar;
 
 it('have defined properties', function () {
     $data = [
         'id' => 1,
         'name' => 'rizky ilham',
         'email' => 'rizyilhampra@gmail.com',
+        'avatar' => (new Avatar(User::factory()->make()))->url(),
     ];
 
     $userData = UserData::from($data);
