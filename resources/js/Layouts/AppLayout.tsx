19195a0766f1870c46118e93b3a5862318fb806d
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-15 12:05:26 +0800

    chore(layout): added FlashMessage component

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -1,3 +1,4 @@
+import { FlashMessage } from '@/components/FlashMessage';
 import { Footer } from '@/Layouts/Partials/Footer';
 import { AppNavbar } from '@/Layouts/Partials/Navbar';
 import { PropsWithChildren } from 'react';
@@ -5,6 +6,7 @@ import { PropsWithChildren } from 'react';
 export function AppLayout({ children }: PropsWithChildren) {
   return (
     <div>
+      <FlashMessage />
       <AppNavbar>{children}</AppNavbar>
       <Footer />
     </div>
