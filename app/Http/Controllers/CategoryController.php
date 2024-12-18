a84e38db603aa9242d59ff5aefae22831f2c9a8a
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-18 15:19:02 +0800

    refactor(CategoryController): update return types for controller methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,46 +10,47 @@ use App\Actions\Category\UpdateCategory;
 use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
 
 final class CategoryController
 {
-    public function index()
+    public function index(): \Inertia\ResponseFactory|\Inertia\Response
     {
         $categories = CategoryCollection::make((new Category())->getPaginated());
 
         return inertia('Category/Index', compact('categories'));
     }
 
-    public function create()
+    public function create(): \Inertia\ResponseFactory|\Inertia\Response
     {
         return inertia('Category/Create');
     }
 
-    public function store(CategoryRequest $request)
+    public function store(CategoryRequest $request): RedirectResponse
     {
         (new CreateCategory($request->validated()))->execute();
 
         return redirect()->route('categories.index');
     }
 
-    public function show(Category $category)
+    public function show(Category $category): \Inertia\ResponseFactory|\Inertia\Response
     {
         return inertia('Category/Detail', compact('category'));
     }
 
-    public function edit(Category $category)
+    public function edit(Category $category): \Inertia\ResponseFactory|\Inertia\Response
     {
         return inertia('Category/Edit', compact('category'));
     }
 
-    public function update(CategoryRequest $request, Category $category)
+    public function update(CategoryRequest $request, Category $category): RedirectResponse
     {
         (new UpdateCategory($category, $request->validated()))->execute();
 
         return redirect()->route('categories.index');
     }
 
-    public function destroy(Category $category)
+    public function destroy(Category $category): RedirectResponse
     {
         (new DeleteCategory($category))->execute();
 
